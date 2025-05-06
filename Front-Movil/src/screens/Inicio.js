265fc6e71331f202b79f92893c4f4007ce3b8598
mel0ss <Mortiz@correo.iue.edu.co>
2025-05-06 13:58:35 -0500

    Botones de inicio con sus respectivas direcciones

--- Front-Movil/src/screens/Inicio.js
+++ Front-Movil/src/screens/Inicio.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Button } from "react-native-web";
-import Login from "./Login";
-import Registro from "./Registro";
+import { StyleSheet, View, Button } from "react-native";
 
-export default function Inicio({navigation}){
+export default function Inicio({navigation}){ 
     return(
         <View style={styles.ContainerInicio}>
-            <Button title="Inicio de sesión" onPress={Login}/>
-            <Button title="Registro" onPress={Registro}/>
+            <Button title="Inicio de sesión" onPress={() => navigation.navigate("Login")} color="#CCD5AE" />
+            <Button title="Registro" onPress={() => navigation.navigate("Registro")} color="#CCD5AE"/>
         </View>
     )
 }
@@ -20,5 +17,6 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    
   });
   
