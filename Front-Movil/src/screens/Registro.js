3737d78f390d121f974284eaa1aa541562a5b3a1
MinPau20 <paula.alvarado0820@gmail.com>
2025-05-07 19:54:06 -0500

    Ajustes en interfaz Registro

--- Front-Movil/src/screens/Registro.js
+++ Front-Movil/src/screens/Registro.js
@@ -4,19 +4,22 @@ import { StyleSheet, View, Text, Button, TextInput } from "react-native";
 export default function Registro({navigation}){
     return(
         <View style={styles.ContainerRegistro}>
-            <View style={styles.ContainerRegistro2}>
-                <Text style={styles.TextoRegistro}>Correo electrónico:</Text>
-                <TextInput style={styles.InputRegistro} placeholder="Correo electrónico"/>
+            <View style={styles.CuadritoRegistro}>
+                <View style={styles.ContainerRegistro2}>
+                    <Text style={styles.TextoRegistro}>Correo electrónico:</Text>
+                    <TextInput style={styles.InputRegistro} placeholder="Correo electrónico"/>
+                </View>
+                <View style={styles.ContainerRegistro2}>
+                    <Text style={styles.TextoRegistro}>Usuario:</Text>
+                    <TextInput style={styles.InputRegistro} placeholder="Usuario"/>
+                </View>
+                <View style={styles.ContainerRegistro2}>
+                    <Text style={styles.TextoRegistro}>Contraseña:</Text>
+                    <TextInput style={styles.InputRegistro} placeholder="Contraseña"/>
+                </View>
+                <Button title="Entrar" onPress={() => navigation.navigate("Dashboard")} color="#CCD5AE"/>
             </View>
-            <View style={styles.ContainerRegistro2}>
-                <Text style={styles.TextoRegistro}>Usuario:</Text>
-                <TextInput style={styles.InputRegistro} placeholder="Usuario"/>
-            </View>
-            <View style={styles.ContainerRegistro2}>
-                <Text style={styles.TextoRegistro}>Contraseña:</Text>
-                <TextInput style={styles.InputRegistro} placeholder="Contraseña"/>
-            </View>
-            <Button title="Entrar" onPress={() => navigation.navigate("Dashboard")} color="#CCD5AE"/>
+            <Text style={styles.MarcaAgua} >Acciones & Gestión S.A.S</Text>
         </View>
     )
 }
@@ -27,23 +30,41 @@ const styles = StyleSheet.create({
         backgroundColor: '#FEFAE0',
         alignItems: 'center',
         justifyContent: 'center',
-      },
-      ContainerRegistro2:{
-          display: 'flex',
-          flexDirection: 'row',
-      },
-      TextoRegistro: {
-          borderWidth: 1,
-          paddingHorizontal: 10,
-          borderRadius: 5,
-          marginBottom: 16,
-      },
-      InputRegistro: {
-          height: 40,
-          borderColor: "#ccc",
-          borderWidth: 1,
-          paddingHorizontal: 10,
-          borderRadius: 5,
-          marginBottom: 16,
-      },  
+    },
+    CuadritoRegistro: {
+        width: 300, 
+        backgroundColor: '#E9EDC9',
+        alignItems: 'center',
+        justifyContent: 'space-around',    
+        borderRadius: 20,
+    },
+    ContainerRegistro2:{
+        marginTop: 15,
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    TextoRegistro: {
+        fontSize: 23,
+        fontFamily: 'Pacifico',
+        color: '#463f3a',
+        paddingHorizontal: 10,
+        borderRadius: 5,
+    },
+    InputRegistro: {
+        height: 40,
+        borderColor: "#6b705c",
+        color: '#CCD5AE',
+        fontFamily: 'Pacifico',
+        borderWidth: 1,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+        marginBottom: 16,
+    },  
+    MarcaAgua: {
+        color: '#CAC080',
+        fontSize: 10,
+        fontFamily: 'Pacifico',
+        position: 'absolute',
+        bottom: 10,
+    }
 })
