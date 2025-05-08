7fcad8cc7395777bb79c4dafd07017cd6716e782
mel0ss <Mortiz@correo.iue.edu.co>
2025-05-07 19:02:27 -0500

    Interfaz de registro ingresos y gastos lista

--- Front-Movil/src/screens/List_Gts_Ing.js
+++ Front-Movil/src/screens/List_Gts_Ing.js
@@ -3,8 +3,35 @@ import { View, Text } from "react-native";
 
 export default function List_Gts_Ing({navigation}){
     return(
-        <View>
-            <Text>Hola</Text>
+        <View style={styles.ContainerLista}>
+            <Text style={styles.TituloLista}>Lista de Gastos e Ingresos</Text>
+            <View>
+
             </View>
+            <Text style={styles.MarcaAgua} >Acciones & Gestión S.A.S</Text>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    ContainerLista: {
+        flex: 1,
+        backgroundColor: '#FEFAE0',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      TituloLista:{
+        fontSize: 42,
+        fontFamily: 'Pacifico',
+        color: '#463f3a',
+        paddingHorizontal: 10,
+        borderRadius: 5,
+    },
+    MarcaAgua:{
+        color: '#CAC080',
+        fontSize: 10,
+        fontFamily: 'Pacifico',
+        position: 'absolute',
+        bottom: 10,
+    }
+});
--- Front-Movil/src/screens/Reg_Ing_Eg.js
+++ Front-Movil/src/screens/Reg_Ing_Eg.js
@@ -1,10 +1,128 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Button } from "react-native";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { TextInput } from "react-native-gesture-handler";
 
-export default function Reg_Ing_Eg({navigation}){
-    return(
-        <View>
-            <Text>Hola</Text>
+function IngresosScreen({navigation}) {
+    return (
+        <View style={styles.ContainerRegistro}>
+        <Text style={styles.TitulosRegistro}>Registros de Ingresos</Text>
+        <View style={styles.CuadritoRegistro}>
+            <View style={styles.ContainerRegistro2}>
+                <Text style={styles.TextoRegistro2}>Descripción: </Text>
+                <TextInput style={styles.InputRegistro} placeholder="Pago de nómina, Trabajo con XXX, etc..." />
+            </View>
+            <View style={styles.ContainerRegistro2}>
+                <Text style={styles.TextoRegistro2} >Cantidad: </Text>
+                <TextInput style={styles.InputRegistro} placeholder="$$$$" />
+            </View>
+            <View style={styles.ContainerRegistro2}>
+                <Text style={styles.TextoRegistro2}>Categoría: </Text>
+                <TextInput style={styles.InputRegistro} placeholder="--" />
+            </View>
+            <View style={styles.ContainerRegistro2}>
+                <Text style={styles.TextoRegistro2}>Fecha: </Text>
+                <TextInput style={styles.InputRegistro} placeholder="DD/MM/AAAA" />
+            </View>
+            <Button title="Registrar" onPress={() => navigation.navigate("Dashboard")} color="#CCD5AE"/>
         </View>
+        <Text style={styles.MarcaAgua} >Acciones & Gestión S.A.S</Text>
+    </View>
+    );
+  }
+
+const Tab = createBottomTabNavigator();
+
+function EgresosScreen({navigation}) {
+    return (
+      <View style={styles.ContainerRegistro}>
+        <Text style={styles.TitulosRegistro}>Registros de Gastos</Text>
+        <View style={styles.CuadritoRegistro}>
+            <View style={styles.ContainerRegistro2}>
+                <Text style={styles.TextoRegistro2}>Descripción: </Text>
+                <TextInput style={styles.InputRegistro} placeholder="Pago de nómina, Trabajo con XXX, etc..." />
+            </View>
+            <View style={styles.ContainerRegistro2}>
+                <Text style={styles.TextoRegistro2} >Cantidad: </Text>
+                <TextInput style={styles.InputRegistro} placeholder="$$$$" />
+            </View>
+            <View style={styles.ContainerRegistro2}>
+                <Text style={styles.TextoRegistro2}>Categoría: </Text>
+                <TextInput style={styles.InputRegistro} placeholder="--" />
+            </View>
+            <View style={styles.ContainerRegistro2}>
+                <Text style={styles.TextoRegistro2}>Fecha: </Text>
+                <TextInput style={styles.InputRegistro} placeholder="DD/MM/AAAA" />
+            </View>
+            <Button title="Registrar" onPress={() => navigation.navigate("Dashboard")} color="#CCD5AE"/>
+        </View>
+        <Text style={styles.MarcaAgua} >Acciones & Gestión S.A.S</Text>
+      </View>
+    );
+}
+
+export default function Reg_Ing_Eg(){
+    return(
+        <Tab.Navigator screenOptions={{
+            tabBarStyle: { backgroundColor: '#FAEDCD' },
+            tabBarIndicatorStyle: { backgroundColor: '#FAEDCD' },
+            tabBarActiveTintColor: '#D4A373',   
+            swipeEnabled: false,                     
+        }}>
+            <Tab.Screen name="Ingresos" component={IngresosScreen} options={{ headerShown: false }} />
+            <Tab.Screen name="Gastos" component={EgresosScreen} options={{ headerShown: false }} />
+        </Tab.Navigator>
     )
 }
+
+const styles = StyleSheet.create({
+    ContainerRegistro: {
+        flex: 1,
+        backgroundColor: '#FEFAE0',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    TitulosRegistro:{
+        fontSize: 42,
+        fontFamily: 'Pacifico',
+        color: '#463f3a',
+        paddingHorizontal: 10,
+        borderRadius: 5,
+    },
+    CuadritoRegistro: {
+        width: 300, 
+        backgroundColor: '#E9EDC9',
+        alignItems: 'center',
+        justifyContent: 'space-around',    
+        borderRadius: 20,
+    },
+    ContainerRegistro2:{
+        marginTop: 15,
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    TextoRegistro2: {
+        fontSize: 23,
+        fontFamily: 'Pacifico',
+        color: '#463f3a',
+        paddingHorizontal: 10,
+        borderRadius: 5,
+    },
+    InputRegistro: {
+        height: 40,
+        borderColor: "#6b705c",
+        color: '#CCD5AE',
+        fontFamily: 'Pacifico',
+        borderWidth: 1,
+        paddingHorizontal: 10,
+        borderRadius: 5,
+        marginBottom: 16,
+    },
+      MarcaAgua:{
+        color: '#CAC080',
+        fontSize: 10,
+        fontFamily: 'Pacifico',
+        position: 'absolute',
+        bottom: 10,
+    }
+})
